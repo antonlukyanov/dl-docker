1b66fdc4a841bd4f07e5b703a4a265e111bbfd71
Anton Lukyanov <anton.lukyanov@gmail.com>
2019-05-06 13:25:10 +0300

    Set mixed precision by default. Trying to fix user permissions.

--- dockercontext/entrypoint.sh
+++ dockercontext/entrypoint.sh
@@ -5,5 +5,5 @@ echo "Starting as user $DLD_USER with UID=$DLD_UID"
 groupadd -r -g ${DLD_GID} ${DLD_USER}
 useradd -r -u ${DLD_UID} --create-home -g ${DLD_USER} -s /bin/bash ${DLD_USER}
 echo "${DLD_USER}:${DLD_USER}" | chpasswd
-chown -R ${DLD_GID}:${DLD_USER} /home/${DLD_USER}
+chown -R ${DLD_UID}:${DLD_GID} /home/${DLD_USER}
 sudo -H -u ${DLD_USER} "$@"
